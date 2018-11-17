75725c43273d6dfe374fe1cc764b2ca9263d2fee
Sachin Dharmapurikar <sachin@dharmapurikar.in>
2018-11-17 20:37:43 +0530

    Refactored code to extract a component.

--- src/index.js
+++ src/index.js
@@ -1,54 +1,9 @@
-// dependencies
-import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-// local dependencies
-import './styles.css'
-
-class App extends Component {
-    constructor(...args){
-        super(...args);
-
-        this.state = {
-            todos: {}
-        };
-    }
-
-    componentDidMount() {
-        this.setState({
-            todos: {
-                1: { id: 1, completed: false, description: 'task 1' },
-                2: { id: 2, completed: true, description: 'task 2' },
-                3: { id: 3, completed: false, description: 'task 3' },
-                4: { id: 4, completed: false, description: 'task 4' }
-            }
-        });
-    }
-
-    toggleTodo(id, e) {
-        e.preventDefault();
-        const todos = _.clone(this.state.todos);
-        todos[id].completed = !todos[id].completed;
-        this.setState({ todos });
-    }
-
-    render() {
-        const { todos } = this.state;
-        return (
-            <ul>
-                {_.map(todos, (todo, id) =>
-                    <li key={id} 
-                        className={todo.completed ? 'completed' : ''}
-                        onClick={(e) => this.toggleTodo(id, e)}
-                    >
-                        {todo.description}
-                    </li>
-                    )}
-            </ul>
-        );
-    }
-}
+// Local dependencies
+import App from './components/App/App';
+import './styles.css';
 
 render(
     <App />,
